4da15d4b1a08eb0d731a4c64d1d16bf713b0b324
Howard Lu <howardlu@igs.com.tw>
2013-10-01 22:39:58 +0800

    Add Excel Wrapper.

--- Accounting/Accounting/Form1.Designer.cs
+++ Accounting/Accounting/Form1.Designer.cs
@@ -28,20 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.excelWrapper1 = new EmbeddedExcel.ExcelWrapper();
             this.SuspendLayout();
+            // 
+            // excelWrapper1
+            // 
+            this.excelWrapper1.Location = new System.Drawing.Point(244, 39);
+            this.excelWrapper1.Name = "excelWrapper1";
+            this.excelWrapper1.Size = new System.Drawing.Size(598, 552);
+            this.excelWrapper1.TabIndex = 0;
+            this.excelWrapper1.ToolBarVisible = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(892, 677);
+            this.Controls.Add(this.excelWrapper1);
             this.Name = "Form1";
             this.Text = "Accounting";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private EmbeddedExcel.ExcelWrapper excelWrapper1;
+
     }
 }
 
--- Accounting/Accounting/Form1.cs
+++ Accounting/Accounting/Form1.cs
@@ -11,14 +11,35 @@ using Microsoft.Office.Interop.Excel;
 using System.Runtime.InteropServices;
 using System.Reflection;
 using System.Runtime.InteropServices.ComTypes;
+using System.IO;
 
 namespace Accounting
 {
     public partial class Form1 : Form
     {
+        #region Properties
+        // Contains a reference to the hosting application
+        private Microsoft.Office.Interop.Excel.Application m_XlApplication = null;
+        // Contains a reference to the active workbook
+        private Workbook m_Workbook = null;
+        #endregion
+
+        #region Constructors
         public Form1()
         {
             InitializeComponent();
         }
+        #endregion
+
+        #region windows form event handler
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.excelWrapper1.OpenFile(Path.Combine(System.Windows.Forms.Application.StartupPath, @"316.xls"));
+            //this.excelWrapper1.OpenFile(@"https://www.google.com.tw/");
+        }
+        #endregion
+
+        #region Methods
+        #endregion
     }
 }
