7a43bf70354a771829b17c2ffc6ac2c60fc3878b
Howard Lu <howardlu@igs.com.tw>
2014-03-02 21:45:28 +0800

    play video test project.

---
+++ KMPTest/KMPTest/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace KMPTest
+{
+    public partial class Form1 : Form
+    {
+        System.Diagnostics.Process Proc;
+        String pn;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Proc = new System.Diagnostics.Process();
+            Proc.StartInfo.FileName = "E:/Program Files/The KMPlayer/KMPlayer.exe";
+            Proc.StartInfo.Arguments = @"D:/Antonio_mcDyess.flv";
+            Proc.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Maximized;
+            Proc.Start();
+            pn = Proc.ProcessName;
+            System.Threading.Thread.Sleep(1000);
+            SendKeys.Send("%{Enter}");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            KMPlayer kmplayer = new KMPlayer();
+            kmplayer.OpenFile("E:/Program Files/The KMPlayer/KMPlayer.exe", "D:/Antonio_mcDyess.flv", 10000);
+        }
+    }
+}
