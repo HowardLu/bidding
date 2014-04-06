e576231c1f530ef7ea485bc0226d430cc2324885
Howard Lu <howardlu@igs.com.tw>
2014-04-06 10:46:16 +0800

    change for mut.

---
+++ SJ Bidding System/SJ Bidding System/Form2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SJ_Bidding_System
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            this.axWindowsMediaPlayer1.URL = @"C:\Wildlife.wmv";
+            this.axWindowsMediaPlayer1.Ctlcontrols.play();
+        }
+
+        private void axWindowsMediaPlayer1_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            if (e.newState == (int)WMPLib.WMPPlayState.wmppsPlaying)
+            {
+                this.axWindowsMediaPlayer1.fullScreen = true;
+            }
+            if (e.newState == (int)WMPLib.WMPPlayState.wmppsStopped)
+            {
+                //this.axWindowsMediaPlayer1.Dispose();
+                //this.Close();
+                //this.Hide();
+                //this.Dispose();
+            }
+        }
+
+        public void CloseForm()
+        {
+            if (this.axWindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsPlaying)
+            {
+                this.axWindowsMediaPlayer1.Dispose();
+            }
+            this.Close();
+        }
+    }
+}
